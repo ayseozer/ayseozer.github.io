18b6d4e9690ff918388151c3ac397b4cf68bc37b
Ayşe ÖZER <114952541+ayseozer@users.noreply.github.com>
2023-01-14 21:27:39 +0300

    Add files via upload

---
+++ Projects/src/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { TodoLayer } from './context/TodoContext';
+import reducer from './context/reducer';
+import { initialState } from './context/reducer';
+
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <TodoLayer initialState={initialState} reducer={reducer}>
+    <App />
+
+    </TodoLayer>
+    
+  </React.StrictMode>
+  
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
